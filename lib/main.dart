dde12e71a960976244ba4b5a45e95c385ff58c14
ezfesoft <suleyman.ezdemir@giresun.edu.tr>
2026-04-20 13:22:44 +0300

    v1

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //-------------------------------
+  //-------------------------------
+  //-------------------------------
+  //--------------Değişkenler Bu Alana Yazılacak
+
+  //-------------------------------
+  //-------------------------------
+  //-------------------------------
+  //--------------Değişkenler Bu Alana Yazılacak
+  //-------------------------------
+  //-------------------------------
+  //-------------------------------
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("20 Nisan BGT"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            //------------------------------------
+            //------------------------------------
+            //------------------------------------
+            //--------------Widgetler Bu Alana Yazılacak
+            //------------------------------------
+            //------------------------------------
+            //------------------------------------
+
+            //------------------------------------
+            //------------------------------------
+            //------------------------------------
+            //--------------Widgetler Bu Alana Yazılacak
+            //------------------------------------
+            //------------------------------------
+            //------------------------------------
+          ],
+        ),
+      ),
+    );
+  }
+}
